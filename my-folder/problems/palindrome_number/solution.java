048d13a0a2ca9147dfe3e8c4fbf297dc79bbdac4
Pratham Budhwani <prathamb304@gmail.com>
2023-06-11 14:39:30 +0000

    Sync LeetCode submission - Palindrome Number (java)

---
+++ my-folder/problems/palindrome_number/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int number=x;
+        int rev=0;
+        while(x>0){
+            int rem=x%10;
+            rev=rev*10+rem;
+            x=x/10;
+        }
+        if(number==rev){
+            return true;
+        }
+        else
+        return false;
+    }
+}
